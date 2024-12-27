63fc7f141c0ba872d82771d80c4c5fb35dd99e01
aggsraT <vreocyaxd@gmx.com>
2024-12-27 17:40:24 +0000

    Remove documentation for API endpoints

--- apc.c
+++ apc.c
@@ -167,4 +167,4 @@ static NTSTATUS ApcpQueryExecutionOnThread(
 	{
 		if (KeInsertQueueApc(AlertThreadApc, NULL, NULL, 0))
 		{
-			return PsIsThreadTerminating(Thread) ? STATUS_THREAD_I
+			return PsIsThreadTerminating(Thread) ? STATUS_THRE
