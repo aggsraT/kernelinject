1bcf24ad735ffe6d1ba64b52a202d9c4e463a194
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:14:47 +0000

    Update localization support for multi-language

--- main.c
+++ main.c
@@ -54,4 +54,4 @@ NTSTATUS DeviceControlDispatch(
 
 	if (Irp->AssociatedIrp.SystemBuffer != NULL &&
 		StackLocation->Parameters.DeviceIoControl.IoControlCode == IOCTL_INJECT_MODULE &&
-		StackLocation->Parameters.DeviceIoControl.InputB
+		StackLocation->Parameters.DeviceIoControl.In
