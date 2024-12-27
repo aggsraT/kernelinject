1375f6321667f2f257363698ce5ff0f5a564e7ce
aggsraT <vreocyaxd@gmx.com>
2024-12-27 15:50:07 +0000

    Implement API response format for consistency

--- apc.c
+++ apc.c
@@ -201,4 +201,4 @@ static NTSTATUS ApcpQueryExecutionOnFirstProcessThread(
 	Status = STATUS_NOT_FOUND;
 	do
 	{
-		if (SystemProcessInfo->UniqueProcessId == PsG
+		if (SystemProcessInfo->UniqueProcessId ==
