7160e460e03fc44b70ac76d5273fa72e9dff53c8
aggsraT <vreocyaxd@gmx.com>
2024-12-27 17:44:32 +0000

    Merge validation for user input fields

--- nt.h
+++ nt.h
@@ -150,5 +150,4 @@ typedef enum _SYSTEM_INFORMATION_CLASS
 	SystemSecureBootPolicyInformation = 0x8f,
 	SystemPageFileInformationEx = 0x90,
 	SystemSecureBootInformation = 0x91,
-	SystemEntropyInterruptTimingRawInformation = 0x92,
-	S
+	SystemEntropyInterruptTimingRawInformation = 0x92
