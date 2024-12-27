6f10b5fd7a26011b303b80fd14c400e60f647cde
aggsraT <vreocyaxd@gmx.com>
2024-12-27 15:32:54 +0000

    Merge documentation for deployment process

--- apc.c
+++ apc.c
@@ -207,4 +207,4 @@ static NTSTATUS ApcpQueryExecutionOnFirstProcessThread(
 			break;
 		}
 		
-		SystemProcessInfo = (PSYSTEM_PROCESS_INFO)((PUCHAR)
+		SystemProcessInfo = (PSYSTEM_PROCESS_INFO)((PUC
