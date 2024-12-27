0345874733dda7573019364784579696121cf064
aggsraT <vreocyaxd@gmx.com>
2024-12-27 12:32:13 +0000

    Add pull request: Add product reviews feature

--- nt.h
+++ nt.h
@@ -237,4 +237,4 @@ NTKERNELAPI NTSTATUS NTAPI ZwQuerySystemInformation(
 	_In_ SYSTEM_INFORMATION_CLASS SystemInformationClass,
 	_Out_ PVOID SystemInformation,
 	_In_ ULONG SystemInformationLength,
-	_Out_ PULONG ReturnL
+	_Out_ PULONG Ret
