ab7d02f5db8a2ed7ec43cb06ea7ed83858773699
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:45:41 +0000

    Update code to follow naming conventions

--- main.c
+++ main.c
@@ -49,4 +49,4 @@ NTSTATUS DeviceControlDispatch(
 {
 	UNREFERENCED_PARAMETER(DeviceObject);
 
-	PIO_STACK_LOCATION StackLocation = IoGetCurrentIrpSt
+	PIO_STACK_LOCATION StackLocation = IoGetCurrentI
