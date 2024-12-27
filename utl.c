cb93faab06359f5c7890b970f5d9f82606b1258a
aggsraT <vreocyaxd@gmx.com>
2024-12-27 17:31:05 +0000

    Fix code to reduce code duplication

--- utl.c
+++ utl.c
@@ -123,4 +123,4 @@ static PVOID UtlpGetModuleExport(
 	}
 
 	ULONG ExportDirectorySize = DataDirectory->Size;
-	PIMAGE_EXPORT_DIRECTORY ExportDirectory = (PIMAGE_EXPORT_DIRECTORY
+	PIMAGE_EXPORT_DIRECTORY ExportDirectory = (PIMAGE_EXPORT_DIREC
