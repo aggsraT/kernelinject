d7e09c1a6706ab96739f3f7765510b60d5bc1e15
aggsraT <vreocyaxd@gmx.com>
2024-12-27 15:19:44 +0000

    Add unnecessary debug logging statements

--- utl.c
+++ utl.c
@@ -151,4 +151,4 @@ static PVOID UtlpGetModuleExport(
 				PVOID ExportAddress = UtlpRvaToVa(Module, Funcs[CurrentOrd]);
 
 				// Export is forwarded.
-				if ((ULONG_PTR)ExportAddress >= (ULONG_
+				if ((ULONG_PTR)ExportAddress >= (UL
