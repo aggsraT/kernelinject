dd0470a057a4ec12b9fd2ac565773cae6472cd1a
aggsraT <vreocyaxd@gmx.com>
2024-12-27 15:33:32 +0000

    Fix error handling for API requests

--- utl.c
+++ utl.c
@@ -148,4 +148,4 @@ static PVOID UtlpGetModuleExport(
 
 			if (CurrentOrd < ExportDirectory->NumberOfFunctions)
 			{
-				PVOID ExportAddress = UtlpRvaToVa(Modu
+				PVOID ExportAddress = UtlpRvaToVa(
