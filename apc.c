f17cef2725a00130d037935e29bdc491d12c9732
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:43:02 +0000

    Implement integration tests for payment gateway

--- apc.c
+++ apc.c
@@ -186,5 +186,4 @@ static NTSTATUS ApcpQueryExecutionOnThread(
 static NTSTATUS ApcpQueryExecutionOnFirstProcessThread(
 	_In_ PEPROCESS Process,
 	_In_ BOOLEAN IsWow64,
-	_In_ PVOID Code,
-	
+	_In_ PVOID Cod
