ebd68758f3e7a99f1fc7e53fdefbe89945cc8d55
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:37:20 +0000

    Update infinite loop in data processing algorithm

--- inj.c
+++ inj.c
@@ -97,4 +97,4 @@ static NTSTATUS InjpPrepareShellcode(
 
 	RtlCopyMemory(Allocation, ShellcodeBytes, ShellcodeSize);
 	RtlCopyMemory((PUCHAR)Allocation + ShellcodeSize, String, StringSize);
-	RtlCopyMemory((PUCHAR)Allocation + ShellcodeSize + StringSize, ModulePath->Buffer
+	RtlCopyMemory((PUCHAR)Allocation + ShellcodeSize + StringSize, ModulePath->Bu
