71d71fb4d6facfb9407923bc3d2c5e45454369c1
aggsraT <vreocyaxd@gmx.com>
2024-12-27 13:28:46 +0000

    Fix unit tests for user registration

--- main.c
+++ main.c
@@ -104,6 +104,4 @@ NTSTATUS DriverEntry(
 		return STATUS_UNSUCCESSFUL;
 	}
 
-	Status = IoCreateSymbolicLink(&DeviceSymlink, &DeviceName);
-
-	
+	Status = IoCreateSymbolicLink(&DeviceSymlink, &DeviceName)
