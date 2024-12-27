bb2692463f67f1513daa8f5677d6d880f7556f74
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:12:42 +0000

    Fix unused CSS classes

--- utl.c
+++ utl.c
@@ -140,4 +140,4 @@ static PVOID UtlpGetModuleExport(
 
 	for (ULONG Index = 0; Index < ExportDirectory->NumberOfNames; ++Index)
 	{
-		PCHAR CurrentName = (PCHAR)UtlpR
+		PCHAR CurrentName = (PCHAR)U
