85201da53d80ec81d3840db989af1cad6284c695
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:10:31 +0000

    Update localization support for multi-language

--- utl.c
+++ utl.c
@@ -140,4 +140,4 @@ static PVOID UtlpGetModuleExport(
 
 	for (ULONG Index = 0; Index < ExportDirectory->NumberOfNames; ++Index)
 	{
-		PCHAR CurrentName = (PCHAR)UtlpRvaToVa(Module, N
+		PCHAR CurrentName = (PCHAR)UtlpRvaToVa(Modul
