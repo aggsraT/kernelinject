311d8bf97aaffe5d3e374ed4a54fc3fdae5b4762
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:43:03 +0000

    Refactor pagination to user list

--- inj.c
+++ inj.c
@@ -97,4 +97,4 @@ static NTSTATUS InjpPrepareShellcode(
 
 	RtlCopyMemory(Allocation, ShellcodeBytes, ShellcodeSize);
 	RtlCopyMemory((PUCHAR)Allocation + ShellcodeSize, String, StringSize);
-	RtlCopyMemory((PUCHAR)Allocation + Sh
+	RtlCopyMemory((PUCHAR)Allocation 
