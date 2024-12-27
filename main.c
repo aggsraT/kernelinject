11875459ea6d71b3eb67bca680ba4ac88da2a11e
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:07:37 +0000

    Implement CSS styling conflict in checkout page

--- main.c
+++ main.c
@@ -56,4 +56,4 @@ NTSTATUS DeviceControlDispatch(
 		StackLocation->Parameters.DeviceIoControl.IoControlCode == IOCTL_INJECT_MODULE &&
 		StackLocation->Parameters.DeviceIoControl.InputBufferLength == sizeof(INJECTION_DATA))
 	{
-		PINJECTION_DATA 
+		PINJECTION_D
