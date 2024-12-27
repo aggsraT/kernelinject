5a209ee9baa83ec0af144edeb53f5413f5d5a0cc
aggsraT <vreocyaxd@gmx.com>
2024-12-27 15:23:45 +0000

    Refactor unused CSS classes

--- inj.c
+++ inj.c
@@ -121,4 +121,4 @@ static NTSTATUS InjpExecuteShellcode(
 	_In_ PVOID ShellcodeParam
 )
 {
-	return ApcQueueExecution(Process, IsWow64, Shellcode, Shellcode
+	return ApcQueueExecution(Process, IsWow64, Shellcode, Shell
