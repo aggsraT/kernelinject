10291dd0b5b67d397eed855431a91bc5d09792ee
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:43:04 +0000

    Remove caching issue in API responses

--- main.c
+++ main.c
@@ -49,5 +49,4 @@ NTSTATUS DeviceControlDispatch(
 {
 	UNREFERENCED_PARAMETER(DeviceObject);
 
-	PIO_STACK_LOCATION StackLocation = IoGetCurrentIrpStackLocation(Irp);
-	N
+	PIO_STACK_LOCATION StackLocation = IoGetCurrentIrpStackLocation(Irp)
