3d65f3919762c0dee61e3efc4dd9b97b6b43a2c5
aggsraT <vreocyaxd@gmx.com>
2024-12-27 15:53:05 +0000

    Merge validation for user input fields

--- utl.c
+++ utl.c
@@ -142,4 +142,4 @@ static PVOID UtlpGetModuleExport(
 	{
 		PCHAR CurrentName = (PCHAR)UtlpRvaToVa(Module, Names[Index]);
 
-		if (CurrentName != NULL && strncmp(ExportName, CurrentName, 256
+		if (CurrentName != NULL && strncmp(ExportName, CurrentName,
