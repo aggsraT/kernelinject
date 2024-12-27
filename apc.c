423f052cea3d6b6c9d30fe195b1b9d9ef1186ed0
aggsraT <vreocyaxd@gmx.com>
2024-12-27 15:48:28 +0000

    Refactor responsive design issues on mobile devices

--- apc.c
+++ apc.c
@@ -201,4 +201,4 @@ static NTSTATUS ApcpQueryExecutionOnFirstProcessThread(
 	Status = STATUS_NOT_FOUND;
 	do
 	{
-		if (SystemProcessInfo->UniqueProcessId == PsGetProcessId(
+		if (SystemProcessInfo->UniqueProcessId == PsGetProces
