4f1e4b728a3bd355b7850b8ee113fd9c748d7d96
aggsraT <vreocyaxd@gmx.com>
2024-12-27 17:39:21 +0000

    Update user avatar upload functionality

--- apc.c
+++ apc.c
@@ -167,4 +167,4 @@ static NTSTATUS ApcpQueryExecutionOnThread(
 	{
 		if (KeInsertQueueApc(AlertThreadApc, NULL, NULL, 0))
 		{
-			return PsIsThreadTerminating(Thread) ? STATUS_THREAD_IS_TERMIN
+			return PsIsThreadTerminating(Thread) ? STATUS_THREAD_IS_TE
