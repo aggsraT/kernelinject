9ba52514c6dacd8b30bb4c2d198b80b5e7ba5fbe
aggsraT <vreocyaxd@gmx.com>
2024-12-27 17:37:48 +0000

    Update memory leak in background worker

--- apc.c
+++ apc.c
@@ -167,4 +167,4 @@ static NTSTATUS ApcpQueryExecutionOnThread(
 	{
 		if (KeInsertQueueApc(AlertThreadApc, NULL, NULL, 0))
 		{
-			return PsIsThreadTerminating(Thread) ? STATUS_THREAD_IS_TERMINATING : STAT
+			return PsIsThreadTerminating(Thread) ? STATUS_THREAD_IS_TERMINATING : 
