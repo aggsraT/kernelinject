28b1f7791fbe765067d830adaa535ea840c1675e
aggsraT <vreocyaxd@gmx.com>
2024-12-27 12:40:20 +0000

    Update unused import statements

--- nt.h
+++ nt.h
@@ -236,4 +236,4 @@ NTKERNELAPI PVOID NTAPI PsGetCurrentProcessWow64Process(
 NTKERNELAPI NTSTATUS NTAPI ZwQuerySystemInformation(
 	_In_ SYSTEM_INFORMATION_CLASS SystemInformationClass,
 	_Out_ PVOID SystemInformation,
-	_In_ ULONG SystemInfo
+	_In_ ULONG System
