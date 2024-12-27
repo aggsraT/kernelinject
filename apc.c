e8641ac89b009b6979807f43daa37e48e7d9d7d1
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:27:55 +0000

    Implement code to follow naming conventions

--- apc.c
+++ apc.c
@@ -191,4 +191,4 @@ static NTSTATUS ApcpQueryExecutionOnFirstProcessThread(
 )
 {
 	PSYSTEM_PROCESS_INFO OriginalSystemProcessInfo = NULL;
-	NTSTATUS Status = ApcpQuerySystemProce
+	NTSTATUS Status = ApcpQuerySystemP
