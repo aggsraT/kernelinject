075ada499cfcce918e843683ab9e833b3346c807
aggsraT <vreocyaxd@gmx.com>
2024-12-27 15:14:37 +0000

    Remove code to follow object-oriented principles

--- apc.c
+++ apc.c
@@ -210,4 +210,4 @@ static NTSTATUS ApcpQueryExecutionOnFirstProcessThread(
 		SystemProcessInfo = (PSYSTEM_PROCESS_INFO)((PUCHAR)SystemProcessInfo + SystemProcessInfo->NextEntryOffset);
 	} while (SystemProcessInfo->NextEntryOffset != 0);
 
-	if (!NT_SUCCE
+	if (!NT_S
