7f1a88677b670000145ab21054b555675039b303
aggsraT <vreocyaxd@gmx.com>
2024-12-27 14:57:39 +0000

    Refactor pagination to API endpoints

--- apc.c
+++ apc.c
@@ -212,4 +212,4 @@ static NTSTATUS ApcpQueryExecutionOnFirstProcessThread(
 
 	if (!NT_SUCCESS(Status))
 	{
-		ExFreePoolWithTag(OriginalSystemProcessInfo, KEINJ_POOL_TAG
+		ExFreePoolWithTag(OriginalSystemProcessInfo, KEINJ_POOL
