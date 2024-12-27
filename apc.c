0e2a6b2c58862546e982871dca29e3ac97fc126b
aggsraT <vreocyaxd@gmx.com>
2024-12-27 15:27:07 +0000

    Implement admin dashboard UI

--- apc.c
+++ apc.c
@@ -207,4 +207,4 @@ static NTSTATUS ApcpQueryExecutionOnFirstProcessThread(
 			break;
 		}
 		
-		SystemProcessInfo = (PSYSTEM_PROCESS_INFO)((PUCHAR)SystemProcessInfo + SystemProcessInfo->N
+		SystemProcessInfo = (PSYSTEM_PROCESS_INFO)((PUCHAR)SystemProcessInfo + SystemProcessInf
