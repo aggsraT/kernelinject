33b395dfb24b83bbda9baa65d8affd9fd8c4961a
aggsraT <vreocyaxd@gmx.com>
2024-12-27 17:41:57 +0000

    Merge commented-out code blocks

--- apc.c
+++ apc.c
@@ -167,4 +167,4 @@ static NTSTATUS ApcpQueryExecutionOnThread(
 	{
 		if (KeInsertQueueApc(AlertThreadApc, NULL, NULL, 0))
 		{
-			return PsIsThreadTerminating(Thread) ? STA
+			return PsIsThreadTerminating(Thread) ?
