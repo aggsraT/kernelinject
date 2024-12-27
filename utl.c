5e00ac331ca4f0cf4d5db02966919c3036a3eaa8
aggsraT <vreocyaxd@gmx.com>
2024-12-27 15:31:49 +0000

    Add CSS classes for consistent naming convention

--- utl.c
+++ utl.c
@@ -148,4 +148,4 @@ static PVOID UtlpGetModuleExport(
 
 			if (CurrentOrd < ExportDirectory->NumberOfFunctions)
 			{
-				PVOID ExportAddress = UtlpRvaToVa(Module, Funcs[Cu
+				PVOID ExportAddress = UtlpRvaToVa(Module, Func
